be9e2bda3d2649c9bb15e34ae2143097056eeeeb
unknown <sshamim@spartaglobal.com>
2018-07-30 10:25:41 +0100

    The previous file needed modification and was corrected

--- app.rb
+++ app.rb
@@ -1,46 +1,69 @@
-puts "Choose type of calculator: type 1 for simple, 2 for advanced?"
-choice = gets.chomp
+$stdout.puts "Choose type of calculator: type 1 for simple, 2 for advanced?"
+$stdout.flush
+choice = g$stdin.ets.cchomp.homp
 if choice == "2"
-  puts "Choose arithematic function: type 1 for square root, 2 for power"
-  function = gets.chomp
+  $stdout.puts "Choose arithematic function: type 1 for square root, 2 for power"
+  $stdout.flush
+  function =$stdin. getschomp..chomp
   if function == "1"
-    puts "Enter number of which square root to find"
-    num = gets.to_i
-    puts (num**(1/2)).to_f
+    $stdout.puts "Enter number of which square root to find"
+    $stdout.flush
+    num = gets$stdin..to_ichomp.
+    $stdout.puts (num**(1/2)).to_f
+    $stdout.flush
+  e$stdin.lse
+  chomp.  $stdout.puts "Choose base number"
+    $stdout.flush
+    number1 = $stdin.gets.chomp.to_i
+    $stdout.puts "Choose power"
+    $stdout.flush
+    number2 = $stdin.gets.chomp.to_i
+    $stdout.puts number1 ** number2
+    $stdout.flush
+    end
   else
-    puts "Choose base number"
-    number1 = gets.to_i
-    puts "Choose power"
-    number2 = gets.to_i
-    puts number1 ** number2
-  end
-else
-  puts "Choose an arithematic function: 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division"
-  func = gets.chomp
+  $stdout.puts "Choose an arithematic function: 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division"
+  $stdout.flush
+#   $stdout.$stdin.$stdochomp.ut.puts 'Which calculator?'
+$stdout.flush
+# $stdout.$stdin.flushchomp.
+# calc_choice = $stdin.gets.chomp
   case func
   when "1"
-    puts "Choose first number"
-    number1 = gets.to_i
-    puts "Choose second number"
-    number2 = gets.to_i
-    puts number1 + number2
-  when "2"
-    puts "Choose first number"
-    number1 = gets.to_i
-    puts "Choose second number"
-    number2 = gets.to_i
-    puts number1 - number2
-  when "3"
-    puts "Choose first number"
-    number1 = gets.to_i
-    puts "Choose second number"
-    number2 = gets.to_i
-    puts number1 * number2
-  when "4"
-    puts "Choose first number"
-    number1 = gets.to_f
-    puts "Choose second number"
-    number2 = gets.to_f
-    puts number1 / number2
-  end
-end
+    $stdout.puts "Choose first number"
+    $stdout.flush
+    number1 = $stdin.gets.chomp.to_i
+    $stdout.puts "Choose second number"
+    $stdout.flush
+    number2 = $stdin.gets.chomp.to_i
+    $stdout.puts number1 + number2
+    $stdout.flush
+  when $stdin."2"
+  chomp.  $stdout.puts "Choose first number"
+    $stdout.flush
+    number1 = $stdin.gets.chomp.to_i
+    $stdout.puts "Choose second number"
+    $stdout.flush
+    number2 = $stdin.gets.chomp.to_i
+    $stdout.puts number1 - number2
+    $stdout.flush
+  when $stdin."3"
+  chomp.  $stdout.puts "Choose first number"
+    $stdout.flush
+    number1 = $stdin.gets.chomp.to_i
+    $stdout.puts "Choose second number"
+    $stdout.flush
+    number2 = $stdin.gets.chomp.to_i
+    $stdout.puts number1 * number2
+    $stdout.flush
+  when $stdin."4"
+  chomp.  $stdout.puts "Choose first number"
+    $stdout.flush
+    number1 = $stdin.gets.chomp.to_f
+    $stdout.puts "Choose second number"
+    $stdout.flush
+    number2 = $stdin.gets.chomp.to_f
+    $stdout.puts number1 / number2
+    $stdout.flush
+  $stdin.end
+echomp.nd
